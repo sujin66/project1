b58e51e8d33e941bb3be43c24971f03db011fa95
sujin66 <99938613+sujin66@users.noreply.github.com>
2022-03-03 09:41:10 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,18 +6,41 @@ $(function(){
         $('.slider').bxSlider();
         });
         
-    $(".mainmenu").mouseover(function () {
-        $('.submenu').stop().slideDown();
-    }).mouseout(function(){
-        $('.submenu').stop().slideUp();
 
-    });
+
+    $('.mainmenu li').each(function(){
+
+        $(this).mouseover(function () {
+
+            var sub = $(this).find('ul')
+            $(sub).stop().slideDown();
+        }).mouseout(function(){
+            $('.submenu').stop().slideUp();
+    
+        });
+
+    });// $('.mainmenu a').each
 
     var now_index;
+    var src_off = $('.map img').attr('src');
+    var src_on = src_off.replace('01','02');
+
+
+
     $('.map img').click(function(){
         now_index = $(this).data('index');
-        $('.loc_img').hide().eq(now_index).show();
+
+        $('.map img').attr('src',src_off);
+        $(this).attr('src',src_on)
+
+        $('.loc_img').hide().eq(now_index).show().hover(function(){
+            $('.round_img div').eq(now_index).fadeIn();
+        }, function(){
+            $('.round_img div').fadeOut();
+        });
+
     });
+
 
 
 
@@ -26,7 +49,6 @@ $(function(){
         now_index = $(this).data('index');
         $('.pnt_img').hide().eq(now_index).show();
     });
-
 
 
 
